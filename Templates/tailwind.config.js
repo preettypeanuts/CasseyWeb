cb873c5fa6442d0d1abd8a8d624e67835e1d34d1
preettypeanuts <mrafly212001@gmail.com>
2023-12-05 11:35:10 +0700

    create banner

--- Templates/tailwind.config.js
+++ Templates/tailwind.config.js
@@ -6,10 +6,22 @@ module.exports = {
       fontFamily: {
         Montserrat: ['Montserrat', 'sans-serif'],
       },
+      keyframes: {
+        scroll: {
+          '0%': {
+            transform: 'translateX(0%)',
+          },
+          '100%': {
+            transform: 'translateX(-100%)',
+          },
+        },
+      },
+      animation: {
+        scroll: 'scroll 15s linear infinite',
+      },
     },
   },
   plugins: [
     require('flowbite/plugin')
   ],
 }
-
