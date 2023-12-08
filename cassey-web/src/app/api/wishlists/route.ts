159937e74f2ec8391918477c1e2740faf0311f83
preettypeanuts <mrafly212001@gmail.com>
2023-12-08 16:46:51 +0700

    user wishlist lookup aggreagate  done

--- cassey-web/src/app/api/wishlists/route.ts
+++ cassey-web/src/app/api/wishlists/route.ts
@@ -1,8 +1,42 @@
 import { decodeToken } from "@/db/helpers/jwt";
 import { addWishList, getWishlists } from "@/db/models/wishlists";
+import { log } from "console";
 import { ObjectId } from "mongodb";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+
+  const authorization = cookies().get("Authorization");
+
+  
+  if (!authorization) {
+    return NextResponse.json(
+      {
+        message: "Authentication failed",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  const accessToken = authorization.value.split(" ")[1];
+  const decodedUser = await decodeToken(accessToken);
+  const userId = decodedUser._id;
+
+const wishes = await getWishlists(userId)
+  return Response.json(
+    {
+      statusCode: 200,
+      message: "Succesfully get wishlist data",
+      data: wishes
+    },
+    {
+      status: 200
+    }
+  )
+}
 
 export async function POST(request: NextRequest) {
   const authorization = cookies().get("Authorization");
@@ -26,7 +60,7 @@ export async function POST(request: NextRequest) {
   const decodedUser = await decodeToken(accessToken);
   const userId = decodedUser._id;
 
-  const wishes = await addWishList({
+  const wishes = await addWishList({ //!ENDPOINT
     userId: new ObjectId(userId),
     productId: new ObjectId(productId),
     createdAt: new Date(),
--- cassey-web/src/db/models/wishlists.ts
+++ cassey-web/src/db/models/wishlists.ts
@@ -1,6 +1,7 @@
 import { ObjectId } from "mongodb";
 import { getDb } from "./user";
 import { ProductModel } from "./products";
+import { log } from "console";
 
 export type WishlistModel = {
   _id: ObjectId;
@@ -11,11 +12,34 @@ export type WishlistModel = {
   wishListUser: ProductModel[];
 };
 
-export const getWishlists = async () => {
+export const getWishlists = async (userId: string) => {
+  console.log(userId, "<<<");
+  
   const db = await getDb();
+
   const wishes = (await db
     .collection("wishlists")
-    .find()
+    .aggregate([
+      {
+        $match: { userId: new ObjectId(userId) },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "userId",
+          foreignField: "_id",
+          as: "user",
+        },
+      },
+      {
+        $lookup: {
+          from: "products",
+          localField: "productId",
+          foreignField: "_id",
+          as: "product",
+        },
+      },
+    ])
     .toArray()) as WishlistModel[];
   return wishes;
 };
