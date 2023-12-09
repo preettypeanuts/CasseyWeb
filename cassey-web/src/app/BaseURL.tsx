03889431f208053393f7469db284c7e04352b9af
preettypeanuts <mrafly212001@gmail.com>
2023-12-09 19:51:22 +0700

    update link

--- cassey-web/src/app/BaseURL.tsx
+++ cassey-web/src/app/BaseURL.tsx
@@ -1,3 +1,3 @@
-const BASE_URL = `http://localhost:3001`
-
+const BASE_URL = `http://localhost:3000`
+// const BASE_URL = `https://cassey-8qpvxvrrv-raflyyys-projects.vercel.app/`
 export default BASE_URL;
--- cassey-web/src/components/Navbar.tsx
+++ cassey-web/src/components/Navbar.tsx
@@ -139,6 +139,14 @@ export const Navbar = ({ children }: { children: ReactNode }) => {
                   Wishlists
                 </a>
               </li>
+              <li>
+                <a
+                  href="/register"
+                  className={`${pathName === '/register' ? 'text-blue-500' : ''} block font-thin py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700`}
+                >
+                  Cassey Club!
+                </a>
+              </li>
             </ul>
           </div>
         </div>
